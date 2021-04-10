0c668b1986ccf37019a45bc749125239c4607baf
Jacek Olszak <jacekolszak@gmail.com>
2021-04-10 20:54:35 +0200

    Add SynchronizedLoans docs

--- database/sync.go
+++ database/sync.go
@@ -9,7 +9,9 @@ import (
 	"github.com/jacekolszak/deebee-loans/service"
 )
 
-// SynchronizedLoans serialize method calls, so the `service` package is free of low-level code such as synchronization
+// SynchronizedLoans serialize service.Loans method calls, so the `service` package is free of low-level code such as synchronization.
+// This approach is simple, but it might be inefficient for large states (more than 100MB of memory), because some methods
+// might block access to state for a significant amount of time (for example SynchronizedLoans.Snapshot may take seconds).
 type SynchronizedLoans struct {
 	mutex sync.Mutex
 	loans *service.Loans
